60fb5f854ff94ee30cdcfd8a4ec263ba1424eed5
Simonbelete <simonbelete@gmail.com>
2022-04-21 20:47:02 +0300

    sample manufacturing addon product creation hidding fields

---
+++ abc_manufacturing/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'abc_manufacturing',
+    'version': '0.1.0',
+    'category': 'Manufacturing/abc',
+    'description': 'Manufacturing helper module',
+    'depends': ['base', 'mrp'],
+    'author': 'Simon Belete',
+    'data': [
+        'views/in_product_template_only_form_view.xml'
+    ]
+}
