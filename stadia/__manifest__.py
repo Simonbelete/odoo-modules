5ef1900c76acdfdabf70a1dc593d4083e8bb9194
Simonbelete <simonbelete@gmail.com>
2022-04-27 10:05:08 +0300

    appraisal main menu

--- stadia/__manifest__.py
+++ stadia/__manifest__.py
@@ -3,5 +3,9 @@
     'version': '0.1.0',
     'category': 'Human Resources',
     'description': 'Stadia Engineering Works Consultant PLC',
-    'depends': ['hr']
+    'depends': ['hr'],
+    'data': [
+        'views/appraisal_views.xml',
+        'views/appraisal_menus.xml'
+    ]
 }
