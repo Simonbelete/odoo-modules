787d100b72ec19f46116a977cfd4ca7fe26059ef
Simonbelete <simonbelete@gmail.com>
2022-04-29 00:41:58 +0300

    appraisal main menu

---
+++ appraisal/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'appraisal',
+    'version': '0.1.0',
+    'category': 'Human Resources',
+    'description': 'Appraisal Form',
+    'depends': ['base', 'hr'],
+    'author': 'Simon Belete',
+    'data': [
+        'views/appraisal_kanban_view.xml',
+        'views/appraisal_views.xml',
+        'views/appraisal_menus.xml'
+    ]
+}
