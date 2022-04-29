4d0095e21415926a9e88137413a3872198168fde
Simonbelete <simonbelete@gmail.com>
2022-04-29 09:43:18 +0300

    appraisal score

--- appraisal/models/__init__.py
+++ appraisal/models/__init__.py
@@ -2,3 +2,4 @@ from . import appraisal
 from . import res_config_settings
 from . import survey
 from . import survey_question
+from . import appraisal_score
--- appraisal/models/appraisal.py
+++ appraisal/models/appraisal.py
@@ -1,4 +1,8 @@
-from odoo import models
+from odoo import models, fields
 
 class Appraisal(models.Model):
     _name = 'appraisal.appraisal'
+
+    employee_id = fields.Many2one('hr.employee', string = 'Employee')
+    survey_id = fields.Many2one('appraisal.survey')
+    appraisal_score_ids = fields.One2many('appraisal.appraisal.score', 'appraisal_id')
