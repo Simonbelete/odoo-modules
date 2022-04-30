51c51c5334f8ef61e915161425924345a1f5795b
Simonbelete <simonbelete@gmail.com>
2022-04-30 08:38:21 +0300

    adding statusbar for appraisal

--- appraisal/models/appraisal.py
+++ appraisal/models/appraisal.py
@@ -3,9 +3,17 @@ from odoo import models, fields, api
 class Appraisal(models.Model):
     _name = 'appraisal.appraisal'
 
+    name = fields.Text(compute="_generate_name_for_appraisal")
     employee_id = fields.Many2one('hr.employee', string = 'Employee')
     survey_id = fields.Many2one('appraisal.survey')
     appraisal_score_ids = fields.One2many('appraisal.appraisal.score', 'appraisal_id')
+    state = fields.Selection(string="Status", required=True, readonly=True, copy=False, tracking=True, selection=[
+        ('draft', 'To Confirm'),
+        ('confirmed', 'Confirmed'),
+        ('done', 'Done')
+    ], default="draft",
+    help="The current state of the appraisal:"
+         "- To Confirm: Newly created appraisal")
 
     def button_confirm_appraisal(self):
         questions = self.env['appraisal.survey.question'].search([('survey_ids', 'in', self.survey_id.id )])
@@ -15,4 +23,9 @@ class Appraisal(models.Model):
                 'survey_question_id': q.id,
                 'score': 0
             })
-        #     self.write({'appraisal_score_ids': [self.appraisal_score_ids, record.id]})
+
+    # TODO: Add date
+    @api.depends('employee_id')
+    def _generate_name_for_appraisal(self):
+        for record in self:
+            record.name = record.employee_id.name
