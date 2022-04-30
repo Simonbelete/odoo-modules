a7b28f00689e36ed0353e93a8a587e2eb233d656
Simonbelete <simonbelete@gmail.com>
2022-04-30 23:06:27 +0300

    starting fresh for appraisal module

--- appraisal/__manifest__.py
+++ appraisal/__manifest__.py
@@ -2,20 +2,7 @@
     'name': 'appraisal',
     'version': '0.1.0',
     'category': 'Human Resources',
-    'description': 'Appraisal Form',
+    'description': 'Appraisal Manager',
     'depends': ['base', 'hr'],
-    'author': 'Simon Belete',
-    'data': [
-        'security/ir.model.access.csv',
-        'views/appraisal_template.xml',
-        'views/appraisal_page_template.xml',
-        'views/res_config_settings_views.xml',
-        'views/appraisal_kanban_view.xml',
-        'views/appraisal_views.xml',
-        'views/appraisal_survey_views.xml',
-        'views/appraisal_survey_views_question.xml',
-        'views/appraisal_trait_views.xml',
-        'views/appraisal_score_views.xml',
-        'views/appraisal_menus.xml'
-    ]
+    'author': 'Simon Belete'
 }
