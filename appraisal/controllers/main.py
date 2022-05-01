6eb2ebfddd6e14f34f5943d026c262d350a3adb1
Simonbelete <simonbelete@gmail.com>
2022-05-01 12:07:22 +0300

    appraisal listing view

--- appraisal/controllers/main.py
+++ appraisal/controllers/main.py
@@ -3,12 +3,30 @@ from odoo import fields, http
 
 class Appraisal(http.Controller):
 
-    def _query(self):
+    def _query(self, question_ids):
         return """
-            SELECT appraisal_appraisal.survey_id
-                FROM appraisal_appraisal
-                
-        """.strip()
+            SELECT appraisal_survey_question.id AS question_id,
+                    appraisal_survey_question.title AS title,
+                    appraisal_survey_question.description AS description,
+                    appraisal_survey_question.sequence AS question_sequence,
+                    appraisal_appraisal_score.id AS score_id
+                FROM appraisal_survey_question
+                LEFT JOIN appraisal_appraisal_score
+                    ON appraisal_survey_question.id = appraisal_appraisal_score.question_id
+                WHERE appraisal_survey_question.id IN (%s)
+                ORDER BY appraisal_survey_question.category_id ASC,
+                    appraisal_survey_question.sequence ASC
+        """ % (','.join(map(str, question_ids)))
+
+
+        # SELECT appraisal_appraisal.survey_id,
+        #             appraisal_survey.title,
+        #             appraisal_survey_question.title
+        #         FROM appraisal_appraisal
+        #         INNER JOIN appraisal_survey
+        #             ON appraisal_appraisal.survey_id = appraisal_survey.id
+        #         INNER JOIN appraisal_survey_question
+        #             ON  appraisal_survey.question_ids = appraisal_survey_question.id
 
         #  INNER JOIN appraisal_survey
         #         INNER JOIN appraisal_survey_question
@@ -20,19 +38,31 @@ class Appraisal(http.Controller):
     @http.route('/appraisal/<string:token>', type="http", website=True)
     def appraisal_display_page(self, token, **kw):
         Appraisal = http.request.env['appraisal.appraisal']
+        AppraisalScore = http.request.env['appraisal.appraisal.score']
         SurveyQuestion = http.request.env['appraisal.survey.question']
         SurveyScore = http.request.env['appraisal.appraisal.score']
         # Get current appraisal
-        # appraisal = Appraisal.search([('token', '=', token)])
+        appraisal = Appraisal.search([('token', '=', token)])
         # Get appraisals survey questions
-        # questions = SurveyQuestion.search([('survey_ids', 'in', (appraisal.id))])
-        # answers = SurveyScore.search([('appraisal_id.token', '=', token)])
-        questions = http.request.cr.execute(self._query())
-        data = http.request.cr.fetchall()
+        # question_ids = appraisal.survey_id.question_ids.ids
+        # http.request.cr.execute(self._query(question_ids))
+        # question_with_answers = http.request.cr.dictfetchall()
+        # print("0000000000000000000000000000000000000000000000")
+        # print(json.dumps(question_with_answers, indent=4, sort_keys=True, default=str))
+
+        question_ids = appraisal.survey_id.question_ids.ids
+        questions = SurveyQuestion.search([('id', 'in', question_ids)])
+        answers = SurveyScore.search([('question_id', 'in', question_ids)])
+        categories = []
+        for question in questions:
+            categories.append(question.category_id)
+        categories = set(categories)
+
         print("0000000000000000000000000000000000000000000000")
-        print(json.dumps(data, indent=4, sort_keys=True, default=str))
+        print(json.dumps(question, indent=4, sort_keys=True, default=str))
         return http.request.render('appraisal.appraisal_page_index',
         {
-            'questions': data,
-            # 'answers': answers
+            'questions': questions,
+            'answers': answers,
+            'categories': categories
         })
