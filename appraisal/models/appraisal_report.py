46a883fb69d343b89e29a0b4025c9536caa51cef
Simonbelete <simonbelete@gmail.com>
2022-05-05 14:50:34 +0300

    appraisal table format

--- appraisal/models/appraisal_report.py
+++ appraisal/models/appraisal_report.py
@@ -8,9 +8,30 @@ class AppraisalReport(models.AbstractModel):
         report = self.env['ir.actions.report']._get_report_from_name('appraisal.appraisal_report')
         # get the records selected for this rendering of the report
         obj = self.env[report.model].browse(docids)
+
+        AppraisalScore = self.env['appraisal.appraisal.score']
+        SurveyQuestion = self.env['appraisal.survey.question']
+        SurveyScore = self.env['appraisal.appraisal.score']
+        # Get current appraisal
+        appraisal = obj
+
+        question_ids = appraisal.survey_id.question_ids.ids
+        questions = SurveyQuestion.search([('id', 'in', question_ids)])
+        answers = SurveyScore.search([('question_id', 'in', question_ids)])
+        categories = []
+        for question in questions:
+            categories.append(question.category_id)
+        categories = set(categories)
+
+        print('ddddddddddddddddddddddddddddddddddddddddddddddd')
+        print(obj.employee_id.name)
+
         # return a custom rendering context
         return {
             'lines': docids,
-            'data': 'data',
-            'da': {'name': 'eg name'}
+            'docs': obj,
+            'appraisal_id': appraisal.id,
+            'questions': questions,
+            'answers': answers,
+            'categories': categories
         }
