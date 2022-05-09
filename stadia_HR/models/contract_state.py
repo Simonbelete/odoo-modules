2d5967cd611a60dbb911b3b369188c7dc060f53e
Simonbelete <simonbelete@gmail.com>
2022-05-09 23:54:44 +0300

    commenting out contract states

--- stadia_HR/models/contract_state.py
+++ stadia_HR/models/contract_state.py
@@ -3,16 +3,17 @@ from odoo import fields, models, _, api
 
 class ContractStat(models.Model):
     _inherit = "hr.contract"
-    asset_taken = fields.Boolean(string="Asset Check")
-    loan_taken = fields.Boolean(string="Loan Check")
+    # asset_taken = fields.Boolean(string="Asset Check")
+    # loan_taken = fields.Boolean(string="Loan Check")
 
-    def expire(self):
-        if self.asset_taken and self.loan_taken:
-            self.state = 'close'
+    # def expire(self):
+    #     for record in self:
+    #         if record.asset_taken and record.loan_taken:
+    #             record.state = 'close'
 
-    def draft(self):
-        self.state = 'open'
+    # def draft(self):
+    #     self.state = 'open'
 
-    def close(self):
-        self.state = 'cancel'
+    # def close(self):
+    #     self.state = 'cancel'
     # state_name = fields.Char(related="stage_id.name", string="Sname")
