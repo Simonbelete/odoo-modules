23c1286f48872a0dac276ace144c32ae48c5d2a5
Simonbelete <simonbelete@gmail.com>
2022-05-10 13:54:33 +0300

    removing previous codes

--- abc_manufacturing/models/in_product_template.py
+++
@@ -1,7 +0,0 @@
-from odoo import models, fields, api
-
-class InProductTemplate(models.Model):
-    _inherit = 'product.template'
-
-    product_type = fields.Many2one('product.type.model', string = 'Product Type', required = True)
-    measurement_uom_id = fields.Many2one('uom.uom', 'Product Unit Measurement')
--- stadia_HR/models/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import models
-from . import employee_detail
-from . import summary_view
-from . import respobsibility
-from . import interview_stage
-from . import contract_state
