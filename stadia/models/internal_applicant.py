34943f83f86e5c15ca35f6781ddd3caa0abc9f7c
Simonbelete <simonbelete@gmail.com>
2022-05-13 12:22:28 +0300

    feat(internal app): required model

--- stadia/models/internal_applicant.py
+++ stadia/models/internal_applicant.py
@@ -3,7 +3,8 @@ from odoo import api, fields, models
 class InternalApplicant(models.Model):
     """ In company applicants list"""
     _name = "stadia.internal.applicant"
+    _rec_name = 'employee_id'
 
-    employee_id = fields.Many2one('hr.employee')
+    employee_id = fields.Many2one('hr.employee', required=True)
     # Select opend job positions
-    job_id = fields.Many2one('hr.job', domain="[('state', '=', 'recruit')]")
+    job_id = fields.Many2one('hr.job', domain="[('state', '=', 'recruit')]", required=True)
