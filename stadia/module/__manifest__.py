8ff07d9527ee282f6c0c44a234e7777c71e55272
Simonbelete <simonbelete@gmail.com>
2022-05-13 10:22:08 +0300

    feat(stadia): depends and data demo

--- stadia/module/__manifest__.py
+++ stadia/module/__manifest__.py
@@ -2,5 +2,10 @@
     'name': 'stadia',
     'version': '0.1.0',
     'category': 'Custome Modules',
-    'author': ''
+    'author': '',
+    'depends': [
+        'base',
+        'hr',
+        'hr_recruitment'
+    ],
 }
