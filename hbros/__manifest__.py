37569f26b4c3a73060ad3b06e6e5bcb8481d3df4
Simonbelete <simonbelete@gmail.com>
2022-05-15 23:13:44 +0300

    feat(data file): product specification

--- hbros/__manifest__.py
+++ hbros/__manifest__.py
@@ -6,6 +6,8 @@
     'data': [
         'data/uom_category_data.xml',
         'data/uom_uom_data.xml',
+        'data/product_type_data.xml',
+        'data/product_specification_data',
         'data/product_category_data.xml',
         'data/product_template_data.xml'
     ]
