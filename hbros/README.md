221685322c1d3db22061ab6d25266142424ad417
Simonbelete <simonbelete@gmail.com>
2022-05-16 16:06:26 +0300

    feat(hbros model): fixed cost

--- hbros/README.md
+++ hbros/README.md
@@ -2,4 +2,6 @@
 
 ## Settings
 
-Activate Unit of Measure on Inventory
+1. Activate Unit of Measure on Inventory
+2. Set ```Product Price``` decimal accuracy to 6
+2. Set ```Product Unit of Measure``` decimal accuracy to 6
--- hbros/models/stock_move.py
+++ hbros/models/stock_move.py
@@ -6,7 +6,8 @@ class StockMove(models.Model):
     # From product template
     standard_price = fields.Float(related='product_tmpl_id.standard_price')
     # Raw * CONSUMTION
-    consumption_cost = fields.Float(compute="_compute_consumption_cost")
+    consumption_cost = fields.Float(compute="_compute_consumption_cost", store=True)
+    total_consumption_cost = fields.Float(compute="_compute_total_consumption_cost", store=True)
 
     # Reference Fields from product.template
     product_type = fields.Many2one(
@@ -21,11 +22,22 @@ class StockMove(models.Model):
         'uom.uom', 'Measurement',
         related="product_tmpl_id.measurement_uom_id", readonly=True
     )
+    production_qty = fields.Float(related="raw_material_production_id.product_qty")
+    # production_qty = fields.Float(related="production_id.product_qty")
 
     @api.depends('standard_price')
     def _compute_consumption_cost(self):
         for record in self:
-            print('-----------------------------')
-            print(record.product_uom_qty)
-            print('-----------------------------')
-            record.consumption_cost = record.standard_price * record.product_uom_qty
+            if(record.state != 'done'):
+                record.consumption_cost = record.standard_price * record.product_uom_qty
+
+    @api.depends('standard_price', 'consumption_cost')
+    def _compute_total_consumption_cost(self):
+        for record in self:
+            print("******************************************")
+            print(record.consumption_cost)
+            print(record.production_qty)
+            print(record.state)
+            print("************************************")
+            if(record.state != 'done'):
+                record.total_consumption_cost = record.production_qty * record.consumption_cost
