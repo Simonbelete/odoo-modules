35b8beb121fc17c41f44f2c3064d86b3238889d5
Simonbelete <simonbelete@gmail.com>
2022-05-23 19:22:59 +0300

    chore(apps): om_hr_payroll

--- hbros/__manifest__.py
+++ hbros/__manifest__.py
@@ -2,7 +2,7 @@
     'name': 'hbros',
     'version': '0.1.0',
     'category': 'Manufacturing',
-    'depends': ['base', 'mrp'],
+    'depends': ['base', 'mrp', 'om_hr_payroll'],
     'data': [
         'security/ir.model.access.csv',
 
