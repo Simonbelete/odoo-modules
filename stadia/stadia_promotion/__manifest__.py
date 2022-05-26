edbbbd8e17fbd4ceff849581d6b5e30091e26083
Simonbelete <simonbelete@gmail.com>
2022-05-26 17:57:24 +0300

    promotion menu and views

--- stadia/stadia_promotion/__manifest__.py
+++ stadia/stadia_promotion/__manifest__.py
@@ -9,6 +9,8 @@
     'data': [
         'security/ir.model.access.csv',
 
-        'views/hr_employee_views.xml'
+        'views/promotion_views.xml',
+        'views/hr_employee_views.xml',
+        'views/promotion_menus.xml'
     ]
 }
