1bf309fe3e2c6bd25dbeb5605e684086c568a469
Simonbelete <simonbelete@gmail.com>
2022-05-27 18:52:56 +0300

    stadia asset module

---
+++ stadia/stadia_asset/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'STADIA Asset',
+    'version': '0.1.0',
+    'category': 'Stadia',
+    'author': '',
+    'depends': [
+        'om_account_asset'
+    ],
+    'data': [
+        'views/asset_menus.xml'
+    ]
+}
