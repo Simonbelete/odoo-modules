ceef5bee03d7cb53c9bc142e21aae9ae720b0511
Simonbelete <simonbelete@gmail.com>
2022-06-01 10:11:10 +0300

    moving job and dept to acquisition folder

--- stadia/stadia_acquisition/__manifest__.py
+++ stadia/stadia_acquisition/__manifest__.py
@@ -9,6 +9,9 @@
     'data': [
         'security/ir.model.access.csv',
 
+        'data/hr_department_data.xml',
+        'data/hr_job_data.xml',
+
         'views/acquisition_views.xml',
         'views/acquisition_menus.xml',
         'views/recommendation_views.xml',
