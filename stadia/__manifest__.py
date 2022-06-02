32de3dbd1483134ec5abd619c93956cf59aabb0a
Simonbelete <simonbelete@gmail.com>
2022-06-02 11:50:00 +0300

    promotion stage view

--- stadia/__manifest__.py
+++ stadia/__manifest__.py
@@ -17,10 +17,12 @@
         'data/res_user_data.xml',
         'data/hr_department_data.xml',
         'data/hr_job_data.xml',
+        'data/promotion_data.xml',
 
         'views/acquisition_views.xml',
         'views/hr_employee_views.xml',
         'views/promotion_views.xml',
+        'views/promotion_stage_views.xml',
         'views/hr_hob_views.xml',
         'views/acquisition_menus.xml',
         'views/promotion_menus.xml'
