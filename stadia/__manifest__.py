c1f14683c7493ad655b17726a9bc2402fb3b4565
Simonbelete <simonbelete@gmail.com>
2022-06-02 08:52:14 +0300

    moving job and dept datas

--- stadia/__manifest__.py
+++ stadia/__manifest__.py
@@ -13,6 +13,8 @@
         'security/acquisition_security.xml',
         
         'data/ir_mail_server_data.xml',
-        'data/res_user_data.xml'
+        'data/res_user_data.xml',
+        'data/hr_department_data.xml',
+        'data/hr_job_data.xml'
     ]
 }
