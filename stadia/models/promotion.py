794e21803ceddec9468a0335aee53e941ac315b4
Simonbelete <simonbelete@gmail.com>
2022-06-02 11:53:49 +0300

    group expand on promotion kanban view

--- stadia/models/promotion.py
+++ stadia/models/promotion.py
@@ -5,4 +5,9 @@ class Promotion(models.Model):
     _name = 'stadia.promotion'
 
     employee_id = fields.Many2one('hr.employee')
-    stage_id = fields.Many2one('stadia.promotion.stage')
+    stage_id = fields.Many2one('stadia.promotion.stage', group_expand="_read_group_state_ids")
+
+    @api.model
+    def _read_group_state_ids(self, stages, domain, order):
+        stage_ids = stages._search([],order=order)
+        return stages.browse(stage_ids)
