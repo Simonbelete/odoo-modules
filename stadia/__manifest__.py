d52262c6d670a7e6f96b729f4319564ec4404a90
Simonbelete <simonbelete@gmail.com>
2022-06-02 08:13:32 +0300

    manifest file setup

---
+++ stadia/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'STADIA Engineering Works Consultant PLC',
+    'version': '0.1.0',
+    'category': 'All',
+    'author': '',
+    'application': True,
+    'depends': [
+        'base',
+        'hr',
+        'hr_recruitment'
+    ],
+    'data': [
+        'data/ir_mail_server_data.xml'
+    ]
+}
