ff3a52bbb4f3078e5307606c1ed317d2273eb683
Simonbelete <simonbelete@gmail.com>
2022-06-05 19:12:49 +0300

    fixing referece field id

--- stadia/models/account_asset.py
+++ stadia/models/account_asset.py
@@ -3,8 +3,8 @@ from odoo import fields, api, models
 class AccountAssetAsset(models.Model):
     _inherit = 'account.asset.asset'
 
-    # asset_movement_ids = fields.One2many('asset.movement')
-    current_movement_id = fields.Many2one('asset.movement', 'asset_id')
+    asset_movement_ids = fields.One2many('asset.movement', 'asset_id')
+    current_movement_id = fields.Many2one('asset.movement')
     asset_movement_count = fields.Integer(compute='_compute_asset_movement_count')
 
     def _compute_asset_movement_count(self):
