cbc772839983b2522bf3cae36166b8662aad30e3
Simonbelete <simonbelete@gmail.com>
2022-06-05 22:14:54 +0300

    asset current movement view

--- stadia/models/account_asset.py
+++ stadia/models/account_asset.py
@@ -4,7 +4,10 @@ class AccountAssetAsset(models.Model):
     _inherit = 'account.asset.asset'
 
     asset_movement_ids = fields.One2many('asset.movement', 'asset_id')
+    # Holds approved asset movement
     current_movement_id = fields.Many2one('asset.movement')
+    current_movement_location_id = fields.Many2one(related="current_movement_id.location_id")
+    current_movement_employee_id = fields.Many2one(related="current_movement_id.employee_id")
     asset_movement_count = fields.Integer(compute='_compute_asset_movement_count')
 
     def _compute_asset_movement_count(self):
--- stadia/models/asset_movement.py
+++ stadia/models/asset_movement.py
@@ -1,20 +1,25 @@
+from multiprocessing.context import assert_spawning
 from odoo import fields, api, models
-
 
 
 class AssetMovement(models.Model):
     _name = 'asset.movement'
 
-    previous_movement_id = fields.Many2one('asset.movement')
     asset_id = fields.Many2one('account.asset.asset', required=True)
+    previous_movement_location_id = fields.Many2one(related='asset_id.current_movement_id.location_id')
+    previous_movement_employee_id = fields.Many2one(related='asset_id.current_movement_id.employee_id')
     state = fields.Selection([
         ('draft', 'Draft'),
         ('approved', 'Approved')
-    ])
+    ], default='draft')
     note = fields.Text()
     date = fields.Date()
-    # Reference to the assets previous movement
-    # current_movement_id = fields.Many2one(related='asset_id.')
 
     location_id = fields.Many2one('asset.location', required=True)
     employee_id = fields.Many2one('hr.employee')
+
+    def action_approve(self):
+        asset = self.env['account.asset.asset'].search([('id', '=', self.asset_id.id)])
+        asset.write({'current_movement_id': self.id})
+        # for record in self:
+        #     record.write({'state', 'approved'})
