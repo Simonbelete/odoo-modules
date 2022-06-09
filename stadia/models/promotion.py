6e728954bb3365e9379e160b17b06a66a3ad33df
Simonbelete <simonbelete@gmail.com>
2022-06-09 09:06:15 +0300

    adding default for many2one field

--- stadia/models/promotion.py
+++ stadia/models/promotion.py
@@ -5,12 +5,17 @@ class Promotion(models.Model):
     _name = 'stadia.promotion'
     _res_name = 'employee_id'
 
+    def _default_stage_id(self):
+        a = self.env['stadia.promotion.stage'].search([('sequence', '=', 1)], limit=1)
+        print('11111111111111111111111111111')
+        return a
+
     # Employee to be promoted
     employee_id = fields.Many2one('hr.employee')
     department_id =  fields.Many2one(related="employee_id.department_id")
     # Previous job id
     job_id = fields.Many2one(related="employee_id.job_id")
-    stage_id = fields.Many2one('stadia.promotion.stage', group_expand="_read_group_state_ids")
+    stage_id = fields.Many2one('stadia.promotion.stage', group_expand="_read_group_state_ids", default=_default_stage_id)
     acquisition_id = fields.Many2one('stadia.acquisition', domain="[('state', '=', 'approved')]")
     recommended_by = fields.Many2one('hr.employee')
     new_designation_job_id = fields.Many2one('hr.job', required=True)
