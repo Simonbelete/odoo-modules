d7d790c8dcce816480f3603204a1c67715367bcb
Simonbelete <simonbelete@gmail.com>
2022-06-13 21:34:38 +0300

    additional fields

--- stadia/models/hr_employee.py
+++ stadia/models/hr_employee.py
@@ -10,6 +10,11 @@ class HrEmployee(models.Model):
     ], required=True)
     promotion_count = fields.Integer(default=0)
     sub_city_id = fields.Many2one('subcity')
+    woreda = fields.Char()
+    house_number = fields.Char()
+    tin_no = fields.Char()
+    bank_account_no = fields.Char()
+    
 
     def action_create_user(self):
         """ Check the employee job position and create user base on that"""
