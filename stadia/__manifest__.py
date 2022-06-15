a777c48a5ab5ac8b43c8e41e1d23375120ff4532
Simonbelete <simonbelete@gmail.com>
2022-06-16 00:23:17 +0300

    badge id with log note

--- stadia/__manifest__.py
+++ stadia/__manifest__.py
@@ -28,6 +28,7 @@
         'data/ir_mail_server_data.xml',
         'data/res_user_data.xml',
         'data/resource_calendar_data.xml',
+        'data/sequence.xml',
         'data/hr_department_data.xml',
         'data/hr_job_data.xml',
         'data/promotion_data.xml',
--- stadia/models/hr_employee_base.py
+++ stadia/models/hr_employee_base.py
@@ -3,6 +3,10 @@ from odoo import fields, api, models
 class HrEmployeeBase(models.AbstractModel):
     _inherit = 'hr.employee.base'
 
+    def _defaultBadgeIdNo(self):
+        return self._generage_full_id()
+
+    badge_id_no = fields.Char(string="ID No", copy=False, default=_defaultBadgeIdNo)
     title = fields.Selection([
         ('ato', 'Ato'),
         ('wt', 'W/t'),
@@ -14,3 +18,19 @@ class HrEmployeeBase(models.AbstractModel):
     house_number = fields.Char()
     tin_no = fields.Char()
     pension_no = fields.Char()
+
+    def action_generate_badge_id(self):
+        id_no = self._generage_full_id()
+        previous_id_no = self.badge_id_no
+        self.write({'badge_id_no': id_no})
+        # Write to log not incase on accidentally clicked
+        message = 'Badge id changed from %s to %s' %(previous_id_no, id_no)
+        self.message_post(body=message)
+        
+    def _generage_full_id(self):
+        id_no = self._generage_next_id()
+        id_no = 'STAD/SID/HO/%s/%s' %(id_no, '21')
+        return id_no
+
+    def _generage_next_id(self):
+        return self.env['ir.sequence'].next_by_code('badge.no.sequence')
