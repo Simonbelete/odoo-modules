1ce207ce73ad27cecec8c06d540ec0ab98d71785
Simonbelete <simonbelete@gmail.com>
2022-06-16 12:23:29 +0300

    fixing family relation

--- stadia/models/hr_employee.py
+++ stadia/models/hr_employee.py
@@ -3,7 +3,7 @@ from odoo import fields, api, models
 class HrEmployee(models.Model):
     _inherit = 'hr.employee'
 
-    family_ids = fields.One2Many('hr.employee.family', 'employee_id')
+    family_ids = fields.One2many('hr.employee.family', 'employee_id')
 
     def action_create_user(self):
         """ Check the employee job position and create user base on that"""
