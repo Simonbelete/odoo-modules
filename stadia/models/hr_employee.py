068f89a36149ddf913abacf360270600c43d484a
Simonbelete <simonbelete@gmail.com>
2022-06-16 12:12:08 +0300

    employee citizenship field

--- stadia/models/hr_employee.py
+++ stadia/models/hr_employee.py
@@ -3,18 +3,6 @@ from odoo import fields, api, models
 class HrEmployee(models.Model):
     _inherit = 'hr.employee'
 
-    # title = fields.Selection([
-    #     ('ato', 'Ato'),
-    #     ('wt', 'W/t'),
-    #     ('wro', 'W/ro')
-    # ], required=True)
-    promotion_count = fields.Integer(default=0)
-    sub_city_id = fields.Many2one('subcity')
-    woreda = fields.Char()
-    house_number = fields.Char()
-    tin_no = fields.Char()
-    pension_no = fields.Char()
-    
     def action_create_user(self):
         """ Check the employee job position and create user base on that"""
         return
--- stadia/models/hr_employee_base.py
+++ stadia/models/hr_employee_base.py
@@ -19,14 +19,26 @@ class HrEmployeeBase(models.AbstractModel):
     tin_no = fields.Char()
     pension_no = fields.Char()
 
+    # def write(self, values):
+    #     res = super(HrEmployeeBase, self).write(values)
+    #     # if(values):
+    #     #     # id_no = values['badge_id_no']
+    #     #     id_no = 'abc'
+    #     #     previous_id_no = self.badge_id_no
+    #     #     self._log_badge_id(previous_id_no, id_no)
+    #     return res
+
     def action_generate_badge_id(self):
         id_no = self._generage_full_id()
         previous_id_no = self.badge_id_no
         self.write({'badge_id_no': id_no})
         # Write to log not incase on accidentally clicked
-        message = 'Badge id changed from %s to %s' %(previous_id_no, id_no)
-        self.message_post(body=message)
+        self._log_badge_id(previous_id_no, id_no)
         
+    def _log_badge_id(self, p_id, n_id):
+        message = 'Badge id changed from %s to %s' %(p_id, n_id)
+        self.message_post(body=message)
+
     def _generage_full_id(self):
         id_no = self._generage_next_id()
         id_no = 'STAD/SID/HO/%s/%s' %(id_no, '21')
