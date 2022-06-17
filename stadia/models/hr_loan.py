64a589b8d9745167b6dc89959b7f420be672b092
Simonbelete <simonbelete@gmail.com>
2022-06-17 14:43:45 +0300

    guarantor latter

--- stadia/models/hr_loan.py
+++ stadia/models/hr_loan.py
@@ -5,3 +5,15 @@ class HrLoan(models.Model):
 
     reason = fields.Text()
     guarantor_id = fields.Many2one('hr.employee')
+    monthly_payment = fields.Float(compute="_compute_monthly_payment")
+    end_payment_date = fields.Date(compute="_compute_end_payment_date")
+    
+    def _compute_end_payment_date(self):
+        if(self.loan_lines):
+            date = max(self.loan_lines.mapped('date'))
+            self.end_payment_date = date
+        else:
+            self.end_payment_date = False
+
+    def _compute_monthly_payment(self):
+        self.monthly_payment = self.loan_amount / self.installment
