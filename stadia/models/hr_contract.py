5516ccd297632b926d5baf774e47c7a43777aed5
Simonbelete <simonbelete@gmail.com>
2022-06-20 11:03:55 +0300

    adding fields to badges

--- stadia/models/hr_contract.py
+++ stadia/models/hr_contract.py
@@ -1,4 +1,6 @@
 from odoo import fields, api, models
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
 
 class HrContract(models.Model):
     _inherit = 'hr.contract'
@@ -14,3 +16,13 @@ class HrContract(models.Model):
     perdime = fields.Monetary(default = 0)
     cost_sharing = fields.Monetary(default = 0)
     work_place_id = fields.Many2one('stadia.workplace', required=True, default=_default_work_place_id)
+
+    # For printing
+    issued_date = fields.Date(compute="_compute_issued_date")
+    expiry_date = fields.Date(compute="_compute_expiry_date")
+
+    def _compute_issued_date(self):
+        self.issued_date = datetime.now()
+
+    def _compute_expiry_date(self):
+        self.expiry_date = datetime.now() + relativedelta(years=1)
