9c50d5b5f79afaf4acaa6792d24fd4e76f19459d
Simonbelete <simonbelete@gmail.com>
2022-06-24 09:21:05 +0300

    promotion more stages

--- stadia/report/manpower_report.py
+++ stadia/report/manpower_report.py
@@ -6,3 +6,10 @@ class ManpowerReport(models.AbstractModel):
     @api.model
     def get_html(self):
         return self.env.ref('stadia.manpower_report_template')._render({})
+
+    @api.model
+    def _get_report_data(self, date_from, date_to):
+        hired_report = []
+        promoted_report = []
+        transfered_report = []
+        if date_from and date_to:
--- stadia/static/src/js/manpower_report.js
+++ stadia/static/src/js/manpower_report.js
@@ -18,13 +18,36 @@ var manpower_report = AbstractAction.extend({
     var self = this;
     self.$('.o_content').append(QWeb.render('o_control_panel', {widget: self}))
     self.$('.o_cp_bottom_left').append(QWeb.render('manpower_report_print_button', {widget: self}))
-    self.$('.o_cp_top_right').append(QWeb.render('manpower_report_form', {widget: self}))
+    var form = self.$('.o_cp_top_right').append(QWeb.render('manpower_report_form', {widget: self}))
+    // Bind Actions
+    form.find('.man_from_date').on('change',this._onchageFromDate.bind(this)).change()
+    form.find('.man_to_date').on('change',this._onchageFromTo.bind(this)).change()
     self.$('.o_content').append(this.html)
   },
   start: async function() {
     await this.get_html()
     this.set_html()
-  }
+  },
+  _onchageFromDate: function(ev) {
+    var date = $(ev.currentTarget).val()
+    if(date){
+      this.given_context.date_from = date;
+      this._reload();
+    }
+  },
+  _onchageFromTo: function(ev) {
+    var date = $(ev.currentTarget).val()
+    if(date){
+      this.given_context.date_to = date;
+      this._reload();
+    }
+  },
+  _reload: function() {
+    var self = this;
+    return this.get_html().then(function () {
+      self.$('.o_content').append(this.html)
+    })
+  },
 })
 
 core.action_registry.add('stadia_manpower_reg', manpower_report);
