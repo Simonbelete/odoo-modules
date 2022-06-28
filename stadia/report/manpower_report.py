cd1ba0e5770af095bce713aa0ee76bb49ef23a82
Simonbelete <simonbelete@gmail.com>
2022-06-28 12:00:12 +0300

    manpower hired report

--- stadia/report/manpower_report.py
+++ stadia/report/manpower_report.py
@@ -17,9 +17,13 @@ class ManpowerReport(models.AbstractModel):
         bold = workbook.add_format({'bold': True})
 
         start_date = data['form']['date_from']
+        start_date = datetime.strptime(start_date, '%Y-%m-%d').date()
         end_date = data['form']['date_to']
+        end_date = datetime.strptime(end_date, '%Y-%m-%d').date()
 
-        employees = self.env['hr.employee'].search([])
+        # Get the employees from contract id
+
+        employees = self.sudo().env['hr.employee'].search([('contract_id', '!=', False)])
 
         sheet.write(0, 0, 'Name of Employe', bold)
         sheet.write(0, 1, 'Position', bold)
@@ -30,33 +34,18 @@ class ManpowerReport(models.AbstractModel):
         sheet.write(0, 6, 'Date of Hired', bold)
 
         col = 1
-        for employee in employees:
-            sheet.write(col, 0, employee.name)
-            sheet.write(col, 1, employee.job_id.name)
-            sheet.write(col, 2, employee.contract_id.wage)
-            sheet.write(col, 3, employee.contract_id.perdime)
-            sheet.write(col, 4, '')
-            sheet.write(col, 5, employee.contract_id.work_place_id.name)
-            col += 1
-
-
-    # @api.model
-    # def _get_report_data(self, date_from, date_to):
-    #     new_hired_data = []
-    #     promotion_data = []
-    #     transfer_data = []
-    #     if (date_from and date_to):
-    #         start_date = datetime.now() #datetime.strptime(date_from, '%Y-%m-%d').date()
-    #         end_date = datetime.strptime(date_to, '%Y-%m-%d').date()
-    #         new_hired_data = self.env['hr.employee'].search([])
-    #         # i.e contract signed stage
-    #         last_stage_id = self.env['stadia.promotion.stage'].search([])
-    #         last_stage_id = max(last_stage_id.mapped('sequence'))
-    #         promotion_data = self.env['stadia.promotion'].search([('id', '=', last_stage_id), ('promotion_type', '=', 'promotion')])
-    #         transfer_data = self.env['stadia.promotion'].search([('id', '=', last_stage_id), ('promotion_type', '=', 'transfer')])
-            
-    #     return {
-    #         'hired': new_hired_data,
-    #         'promoted': promotion_data,
-    #         'transfer': transfer_data
-    #     }
+        for employee in employees:             
+            ## TODO: improve efficiency
+            if(not employee.first_contract_date):
+                continue
+            print('999999999999999999999999')
+            print(employee.first_contract_date)
+            if(employee.first_contract_date >= start_date and employee.first_contract_date <= end_date):
+                sheet.write(col, 0, employee.name)
+                sheet.write(col, 1, employee.job_id.name)
+                sheet.write(col, 2, employee.contract_id.wage)
+                sheet.write(col, 3, employee.contract_id.perdime)
+                sheet.write(col, 4, '')
+                sheet.write(col, 5, employee.contract_id.work_place_id.name)
+                sheet.write(col, 6, employee.first_contract_date.strftime('%m/%d/%Y'))
+                col += 1
