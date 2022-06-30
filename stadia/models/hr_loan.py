7750b676abaf5d1cf3cc52b8b3e4f97c18a465f6
Simonbelete <simonbelete@gmail.com>
2022-06-30 09:22:01 +0300

    adding clickable kanban

--- stadia/models/hr_loan.py
+++ stadia/models/hr_loan.py
@@ -1,4 +1,6 @@
 from odoo import fields, api, models
+from dateutil import relativedelta
+from datetime import datetime
 
 class HrLoan(models.Model):
     _inherit = 'hr.loan'
@@ -9,6 +11,20 @@ class HrLoan(models.Model):
     end_payment_date = fields.Date(compute="_compute_end_payment_date")
     no_months = fields.Integer(default=0, required=True)
     salary = fields.Monetary(related='employee_id.contract_id.wage')
+    first_contract_date = fields.Date(related='employee_id.contract_id.first_contract_date')
+    service_years = fields.Float(compute="_compute_service_years")
+
+    @api.depends('employee_id')
+    def _compute_service_years(self):
+        self.ensure_one()
+        print('333333333333333333333333333333')
+        print(datetime.today())
+        print(self.first_contract_date)
+        if(self.first_contract_date):
+            diff = relativedelta.relativedelta(datetime.today(), self.first_contract_date)
+            self.service_years = diff.years
+        else:
+            self.service_years = 0
 
     @api.onchange('no_months', 'employee_id')
     def onchange_no_months(self):
