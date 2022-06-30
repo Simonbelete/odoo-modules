4757222e95569bba3874c42e23dbba8ee33cb17e
Simonbelete <simonbelete@gmail.com>
2022-06-30 10:30:39 +0300

    finxg employee count

--- stadia/models/work_place.py
+++ stadia/models/work_place.py
@@ -10,7 +10,12 @@ class WorkPlace(models.Model):
         ('project', 'Project')
     ], default='project')
     is_favorite = fields.Boolean(default=False)
-    employee_count = fields.Integer(default=0)
+    employee_count = fields.Integer(default=0, compute="_compute_employee_count")
+
+    def _compute_employee_count(self):
+        self.ensure_one()
+        count = self.env['hr.employee'].search_count([('contract_id.work_place_id', '=', self.id)])
+        self.employee_count = count
 
     def action_open(self):
         return {
