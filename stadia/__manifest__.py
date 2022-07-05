ecbed60a8e7d78d2f4f7b7addb62a7046dcc6980
demelask <demelash1452@gmail.com>
2022-07-05 12:21:02 +0300

    delete hr_filter.xml

--- stadia/__manifest__.py
+++ stadia/__manifest__.py
@@ -97,7 +97,6 @@
         'views/hr_employee_menus.xml',
         'views/hr_resume_line_views.xml',
         'views/appraisal_views.xml',
-        'views/hr_filter.xml',
 
         'website/appraisal_website.xml'
     ],
