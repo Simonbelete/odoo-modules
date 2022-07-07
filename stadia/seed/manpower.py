4831193d053d0c34cdee384abb226354d7907ef6
Simonbelete <simonbelete@gmail.com>
2022-07-07 12:15:01 +0300

    seed with many2one field

--- stadia/seed/manpower.py
+++ stadia/seed/manpower.py
@@ -4,7 +4,7 @@ import xmlrpc.client
 url = 'http://localhost:8069'
 db = 'stadia'
 user = 'admin'
-password = 'be3327e92d72240272e41918c281f6716a74ce4c'
+password = '354c68c9432e839044c3206e0a2412814329175e'
 
 common = xmlrpc.client.ServerProxy('{}/xmlrpc/2/common'.format(url))
 uid = common.authenticate(db, user, password, {})
@@ -20,15 +20,32 @@ models = xmlrpc.client.ServerProxy('{}/xmlrpc/2/object'.format(url))
 with open('manpower.csv') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
     line_count = 0
-    for row in csv_reader:
-        if(line_count == 0):
-            continue
-
-        employee_id = models.execute_kw(db, uid, password, 'hr.employee', 'create', [{
-            'name': row[1].strip(),
-            'gender': row[2].strip(),
-            'job_id': 1
+    job =  models.execute_kw(db, uid, password, 'hr.department', 'search', [[['name', '=', 'Management']]])
+    employee_id = models.execute_kw(db, uid, password, 'hr.employee', 'create', [{
+            'name': 'Ababe',
+            'gender': 'male',
+            'job_id': job[0],
+            # 'department_id': (1, False, 1)
         }])
-        print(employee_id)
-        break
-        line_count += 1
+    # job_id = models.execute_kw(db, uid, password, 'hr.employee', 'write', [{
+    #         'job_id': 
+    #     }])
+    print(job)
+    print(employee_id)
+
+    # for row in csv_reader:
+    #     if(line_count == 0):
+    #         continue
+        
+    #     if(line_count > 1):
+    #         break
+        
+    #     print('1111111111111111111')
+
+    #     employee_id = models.execute_kw(db, uid, password, 'hr.employee', 'create', [{
+    #         'name': row[1].strip(),
+    #         'gender': row[2].strip(),
+    #         'job_id': 1
+    #     }])
+    #     print(employee_id)
+    #     line_count += 1
