cb40d959b8374d463421dc908c9b0b8fadb95b9a
Simonbelete <simonbelete@gmail.com>
2022-07-09 12:56:32 +0300

    adding mail on asset location

--- stadia/models/asset_location.py
+++ stadia/models/asset_location.py
@@ -2,6 +2,7 @@ from odoo import fields, api, models
 
 class AssetLocation(models.Model):
     _name = 'asset.location'
+    _inherit = ['mail.thread', 'mail.activity.mixin']
     _sql_constraints = [
         ('name_unique', 'unique (name)', 'Asset location name already exists' )
     ]
