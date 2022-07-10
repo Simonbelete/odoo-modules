e11d7f655121160201cb5c79882836276be1a498
Simonbelete <simonbelete@gmail.com>
2022-07-10 19:53:22 +0300

    squasing hr employee and base codes

--- stadia/models/__init__.py
+++ stadia/models/__init__.py
@@ -2,7 +2,6 @@ from . import acquisition
 from . import promotion
 from . import promotion_stage
 from . import hr_job
-from . import hr_employee
 from . import hr_applicant
 from . import hr_attendance
 from . import regularization
@@ -14,7 +13,7 @@ from . import sub_city
 from . import education
 from . import survey_user
 from . import work_place
-from . import hr_employee_base
+from . import hr_employee
 from . import hr_employee_family
 from . import hr_loan
 from . import city
