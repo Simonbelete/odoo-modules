2574f398a142d7cd1b6e0d905abe8a3888b3d16f
Simonbelete <simonbelete@gmail.com>
2022-07-15 19:47:08 +0300

    chore(setup): basic manifest setup

---
+++ oma/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    "name": "OMA Backend Theme",
+    "summary": "Odoo Basic Backend Theme",
+    "version": "0.1.0",
+    "category": "Themes/Backend",
+    "license": "MIT",
+    "author": "Simon Belete",
+    "website": "https://simonbelete.com",
+    "contributors": [
+        "Simon Belete <simonbelete@gmail.com>"
+    ],
+    "depends": [
+        "base_setup"
+    ],
+    "application": False
+}
