94f14924f279cbc8d06227fcbccc18a74ad4e283
Simonbelete <simonbelete@gmail.com>
2022-07-20 00:31:45 +0300

    eg jspdf to pdfjs view

--- esign/static/src/js/pdf_edit.js
+++ esign/static/src/js/pdf_edit.js
@@ -18,12 +18,42 @@ var pdf_edit = AbstractAction.extend({
     const doc = new jspdf.jsPDF();
 
     doc.text("Hello world!", 10, 10);
-    doc.save("a4.pdf")
+    doc.text("ListBox:", 10, 115);
+    var listbox = new jspdf.AcroForm.ListBox();
+    listbox.edit = false;
+    listbox.fieldName = "ChoiceField2";
+    listbox.topIndex = 2;
+    listbox.Rect = [50, 110, 30, 10];
+    listbox.setOptions(["c", "a", "d", "f", "b", "s"]);
+    listbox.value = "s";
+    doc.addField(listbox);
 
-    // var loadingTask = pdfjsLib.getDocument('esign/static/src/js/helloworld.pdf');
-    // loadingTask.promise.then(function(pdf) {
-    //   // you can now use *pdf* here
-    // });
+    url = doc.output('bloburi')
+
+    var loadingTask = pdfjsLib.getDocument(url);
+    loadingTask.promise.then(function(pdf) {
+
+      var pageNumber = 1;
+      pdf.getPage(pageNumber).then(function(page) {
+        var scale = 1.5;
+        var viewport = page.getViewport({scale: scale});
+
+        var canvas = document.getElementById('pdf-js-viewer');
+        var context = canvas.getContext('2d');
+        canvas.height = viewport.height;
+        canvas.width = viewport.width;
+
+        // Render PDF page into canvas context
+        var renderContext = {
+          canvasContext: context,
+          viewport: viewport
+        };
+        var renderTask = page.render(renderContext);
+        renderTask.promise.then(function () {
+          console.log('Page rendered');
+        });
+      })
+    });
 
 		// End
 
