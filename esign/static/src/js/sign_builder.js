3907575f2bc6dfe5c2c39d654ff2b4cfb7fccd88
Simonbelete <simonbelete@gmail.com>
2022-07-22 23:17:34 +0300

    pdf view full height

--- esign/static/src/js/sign_builder.js
+++ esign/static/src/js/sign_builder.js
@@ -7,8 +7,15 @@ odoo.define('esign.sign_builder', function (require) {
 
     var sign_builder = AbstractAction.extend({
         start: function () {
-           
-        }
+            this.renderBody();
+        },
+        setupGlobalVars: function () {
+            this.$container = this.$('#container')[0];
+        },
+        renderBody: function () {
+            this.$('.o_content').append(QWeb.render('sign_builder_body', {Widget: this}))
+            this.$('.o_content').css({'overflow': 'hidden'})
+        },
     });
 
     core.action_registry.add('sign_builder_registry', sign_builder);
